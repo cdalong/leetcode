bbe65bdb9c4233f0431414b67fbbc35e1ee34386
Cam Long <cdalong@uvic.ca>
2017-09-30 17:55:52 -0700

    JudgeCircle completed

---
+++ leetcode/src/JudgeCircle.java
@@ -0,0 +1,45 @@
+public boolean judgeCircle(String moves) {
+        
+        int xplane = 0;
+        int yplane = 0;
+        
+        
+        for (int i = 0; i < moves.length(); i++){
+            
+            char c = moves.charAt(i);
+            
+            
+            if (c == 'U'){
+                
+                yplane ++;
+                
+            }
+             if (c == 'D'){
+                
+                yplane --;
+                
+            }
+             if (c == 'L'){
+                
+                xplane --;
+                
+            }
+             if (c == 'R'){
+                
+                xplane ++;
+                
+            }
+            
+        }
+        
+        if(xplane == 0 && yplane == 0){
+            
+            return true;
+            
+        }
+        else{
+            
+            return false;
+        }
+        
+    }
